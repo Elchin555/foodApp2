752a9bf188f960af8ec1df35546d43d2f8f30b73
elcin1040 <ebil.rehimov@mail.ru>
2021-04-01 05:12:59 +0400

    2

---
+++ components/AppFooter.vue
@@ -0,0 +1,8 @@
+<template>
+  <footer>
+    <p>Copyright bla blaa blaa.</p>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,40 +1,47 @@
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
+  
+  mode: "universal",
+  
+  target: "static",
+  
   head: {
     title: 'just-test-nuxt',
-    htmlAttrs: {
-      lang: 'en'
-    },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+    meta: [{
+        charset: "utf-8"
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1"
+      },
+      {
+        hid: "description",
+        name: "description",
+        content: "Demo of Nuxt and Vue"
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    link: [{
+        rel: "icon",
+        type: "image/x-icon",
+        href: "/favicon.ico"
+      },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css2?family=Mulish:wght@300&family=Poppins:wght@600&display=swap"
+      }
     ]
   },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
+  /*
+   ** Global CSS
+   */
+  css: ["~/assets/main.scss"],
+  
+  plugins: ["~/plugins/getfood.server.js", "~/plugins/vuelidate.js"],
+  
   components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  
+  buildModules: [],
+  
+  modules: [],
+  
+  build: {}
+};
